3171e6d07e96e97ce5e9b279aa227b537d27312c
Tirth077-coder <alice721082@gmail.com>
2026-07-05 00:28:33 +0530

    odd_even

--- If_else/odd_even.c
+++ If_else/odd_even.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    // Take a positve integer as input and tell if it is even or odd
-int x;
-printf("ENTER A POSITIVE INTEGER : ");
-scanf("%d",&x);
-if(x%2==0){
-printf("THE NUMBER IS EVEN");
-}
-if(x%2!=0){
-    printf("THE NUMBER IS ODD");
-}
+    int x;
+    printf("ENTER THE POSITIVE INTGER :");
+    scanf("%d",&x);
+    if(x%2==0){
+     printf("THE NUMBER IS EVEN");
+    }
+    if(x%2!=0){
+        printf("THE NUMBER IS ODD");
+    }
     return 0;
 }
